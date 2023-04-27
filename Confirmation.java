c16dc727bfb0761c81eb882469b25347d7bfd5c7
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-04-27 17:20:11 -0500

    Add files via upload

---
+++ Confirmation.java
@@ -0,0 +1,19 @@
+import java.util.UUID;
+import java.io.*;
+
+
+public class Confirmation extends WordChoice{
+    private String confirm_id;
+
+    public static void showcase_word(){
+
+        // Access the variable x from ClassA
+        WordChoice ed = new WordChoice();
+        
+        System.out.println("This is a confirmation for your word you inputted today!"+ ed.word);
+
+}}
+    /*public void Choice(){    
+        String confirm_id = UUID.randomUUID().toString();
+        System.out.println(confirm_id);
+    }*/
